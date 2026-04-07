d20b73314bad86c0c51e209ac3068a0cf84ed9b6
Abel Lucas <abelucas009@gmail.com>
2026-04-06 21:27:48 -0300

    add porta

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+import os
 from flask import Flask, render_template  
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("index.html") 
+    return render_template("index.html") 
 
 @app.route("/Galeria")
 def gallery():
-  return render_template("gallery.html") 
+    return render_template("gallery.html") 
 
 @app.route("/Atletas")
 def athletes():
-  return render_template("Athletes.html")
+    return render_template("Athletes.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
